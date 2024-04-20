167d4f4c3f5171ea58da8de1e5b479d1ae9ac2c2
Berk9626 <berkberkmen12@gmail.com>
2024-04-20 18:22:53 +0300

    program.cs reworked and carter libr. added only for catalog.api. We tested and api worked exactly

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,11 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
 //add services to the container
+builder.Services.AddCarter();//Carter maps the routes defined in the ICarter modeule implementation
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);// we gonna register all services into this project, into the 
+    //... mediator class library
+
+});
 
 var app = builder.Build();
 
 //configure the http request pipeline
 
 
+app.MapCarter();
 
 app.Run();
