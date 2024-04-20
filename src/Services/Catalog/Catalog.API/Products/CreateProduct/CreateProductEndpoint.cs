cc78054fba9db2d70637139b976bcf164025655b
Berk9626 <berkberkmen12@gmail.com>
2024-04-20 17:58:53 +0300

    rework endpoints and added, carter, mapster libraries with GlobalUsing

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,7 +1,35 @@
-﻿namespace Catalog.API.Products.CreateProduct
-{//presentation :)
-    public class CreateProductEndpoint //
-    {
+﻿
+namespace Catalog.API.Products.CreateProduct
+{//presentation :) //bu telefonda yer alan şekildeki request ve response öğeleri
 
+
+    public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);//this will be including all items in the product model class
+    public record CreateProductResponse(Guid Id);
+    public class CreateProductEndpoint : ICarterModule //carter kullanımı, burda aslında post methodu için ele aldık.
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>(); //mapped
+                //bizim mediatr requiring command obj. in order to trigger our command handler
+                var result = await sender.Send(command);//send mediatr
+                //now start the medaitor and triggered the handler class...
+                var response = result.Adapt<CreateProductResponse>();
+                return Results.Created($"/products/{response.Id}", response);
+            })
+                .WithName("CreateProduct")
+                .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Create Product")
+                .WithDescription("Create Product");
+           
+        }
     }
+
+    //biz burda request ürettik. Ardından mediatr'yi kullandık ve mediatr handler classı triggerladık. Ardından değerleri 
+    //request objectten, handler command objecte geçirdik(createProductCommandHandler) Burdan geçtikten sonra da mappinge ihtiyaç oldu
+    //şekilde requestten commandquery'e geçiş için mapping yapmamız gerektiğini anladık.how we can convert to request to command obj ?
+    //(Mapping libr.) = Mapster we will use mapster in order to convert our request module to the mediator command object and vice verca
+    //
 }
--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,22 +1,50 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
+
 
 namespace Catalog.API.Products.CreateProduct
 {//application logic layer
     //record: genellikle veri taşıyan, sorgulanan veya tutulan nesnelerin tanımlanmasında kullanılır. record ifadesi, genellikle sadece veri tutan ve değişmez (immutable) nesnelerin tanımlanmasında tercih edilir.
 
 
+
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
-        : IRequest<CreateProductResult>;
+        : ICommand<CreateProductResult>;
+
+
     public record CreateProductResult(Guid Id); 
 
-    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
     //bizim command ve result clasları üretilecek
     //kelimesi bir türün veya üyenin yalnızca tanımlandığı derleme biriminde erişilebilir olduğunu belirtir. Bu, kodunuzun daha iyi modülerlik ve izolasyon sağlamasına yardımcı olabilir.
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
             //bu method perform the bussiness logic to create a product
-            throw new NotImplementedException();
+            //create product entity
+            //save to database
+            //return result
+
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+
+
+            };
+            //save to db
+            
+
+            return new CreateProductResult(Guid.NewGuid());
+
+
+
+
+           
         }
     }
 }
