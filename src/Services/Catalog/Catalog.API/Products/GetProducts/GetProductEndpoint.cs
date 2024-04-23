9e0b98c27fff7bf0b4abb75fc777d6d7aa4577d2
Berk9626 <berkberkmen12@gmail.com>
2024-04-23 17:17:11 +0300

    getproductendpoint added, and keep building catalog step by step

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductEndpoint.cs
@@ -0,0 +1,37 @@
+﻿
+using Catalog.API.Products.CreateProduct;
+
+namespace Catalog.API.Products.GetProducts
+{
+
+    //public record GetProductRequest();
+    public record GetProductResponse(IEnumerable<Product> Products);
+
+
+    public class GetProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", async (ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductsQuery());
+                var response = result.Adapt<GetProductResponse>();// mapster in that way we can directly pass result object
+                //to do response object, paramters name should be exact the name
+
+
+                return Results.Ok(result);
+                
+            })
+                .WithName("CreateProduct")
+                .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Create Product")
+                .WithDescription("Create Product");
+
+
+
+
+            
+        }
+    }
+}
